22ee62d7f746d09e9d8f65ff93c9b4b12039bed8
Stachozaur <stachowicz.karol@gmail.com>
2020-10-29 01:24:43 +0100

    UI print message and display inventory functions

---
+++ src/Codecool.DungeonCrawl/UI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codecool.DungeonCrawl
+{
+    public static class UI
+    {
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void DisplayInventory(Dictionary<Item, int> inventory)
+        {
+            foreach ((Item key, int count) in inventory)
+            {
+                Console.WriteLine($"{key}: {count}");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
