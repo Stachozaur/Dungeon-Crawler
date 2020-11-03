ff8472e8a74dab63569357489f51a322f73b9727
Arkadiusz Kowalczyk <akowalczyk061@gmail.com>
2020-11-03 10:47:53 +0100

    Added Key Item

---
+++ src/Codecool.DungeonCrawl/Items/DoorKeyType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codecool.DungeonCrawl.Items
+{
+    public enum DoorKeyType
+    {
+        Red,
+        Blue,
+        Yellow
+    }
+}
